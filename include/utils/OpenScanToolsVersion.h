9417acf45a61e2305eb30700705c620a28bc8799
Robin-Kervadec <robin.kervadec@gmail.com>
2025-05-16 19:40:47 +0200

    Version 1.0.9

--- include/utils/OpenScanToolsVersion.h
+++ include/utils/OpenScanToolsVersion.h
@@ -2,7 +2,7 @@
 
 #define TEXT_OPENSCANTOOLS QString::fromStdString("OpenScanTools")
 #define OPENSCANTOOLS_YEAR "2025"
-#define BUILD_DATE "28/04/2025"
-#define OPENSCANTOOLS_VERSION "1.0.8"
-#define W_OPENSCANTOOLS_VERSION L"1.0.8"
+#define BUILD_DATE "16/05/2025"
+#define OPENSCANTOOLS_VERSION "1.0.9"
+#define W_OPENSCANTOOLS_VERSION L"1.0.9"
 #define OPENSCANTOOLS_CONTACT_URL "https://www.openscantools.com/"
