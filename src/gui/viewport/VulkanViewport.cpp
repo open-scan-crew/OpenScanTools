29255cb920adfa87308fa1ea4d8e5a8a84d100e7
YanKoch <74600850+Yan-Koch@users.noreply.github.com>
2025-12-16 19:04:33 +0100

    Tighten double-click examine detection (#68) - 4th trial
    4th trial - was still not working...

--- src/gui/viewport/VulkanViewport.cpp
+++ src/gui/viewport/VulkanViewport.cpp
@@ -586,7 +586,7 @@ void VulkanViewport::mouseDoubleClickEvent(QMouseEvent* _event)
     m_MI.lastY = _event->pos().y();
 
     const bool samePositionAsLastClick = glm::ivec2(m_MI.lastX, m_MI.lastY) == m_lastClickPosition;
-    m_doubleClickWithoutMovement = (_event->button() == Qt::LeftButton && !m_mouseMovedSinceLastClick
+    m_doubleClickWithoutMovement = (_event->button() == Qt::LeftButton
         && (!m_hasLastClickPosition || samePositionAsLastClick));
 
     m_lastClickPosition = { m_MI.lastX, m_MI.lastY };
