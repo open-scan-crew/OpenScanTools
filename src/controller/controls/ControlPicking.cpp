3bdc6e83d59e6f704551412ec8648ef244b8fdf8
YanKoch <74600850+Yan-Koch@users.noreply.github.com>
2026-03-18 16:08:52 +0100

    Use robust visible-scan bbox for min/max temperature search

--- src/controller/controls/ControlPicking.cpp
+++ src/controller/controls/ControlPicking.cpp
@@ -1,15 +1,69 @@
 #include "controller/controls/ControlPicking.h"
 #include "controller/Controller.h"
+#include "controller/ControllerContext.h"
+#include "controller/IControlListener.h"
+#include "controller/controls/ControlFunction.h"
 #include "controller/functionSystem/FunctionManager.h"
 #include "controller/messages/FullClickMessage.h"
 #include "gui/GuiData/GuiDataMeasure.h"
+#include "gui/GuiData/GuiDataMessages.h"
 #include "gui/GuiData/GuiDataRendering.h"
+#include "gui/UnitConverter.h"
+#include "gui/texts/ContextTexts.hpp"
 #include "utils/Logger.h"
+#include "utils/ProjectColor.hpp"
 
 #include "models/graph/GraphManager.h"
+#include "models/graph/TagNode.h"
+#include "models/pointCloud/PointXYZIRGB.h"
+#include "pointCloudEngine/OctreeRayTracing.h"
+#include "pointCloudEngine/TlScanOverseer.h"
+#include "tls_def.h"
+
+#include <QObject>
+#include <limits>
+#include <vector>
 
 namespace control::picking
 {
+    namespace
+    {
+        GeometricBox makeGeometricBoxFromBoundingBox(const BoundingBoxD& bbox)
+        {
+            std::vector<glm::dvec3> corners;
+            corners.reserve(8);
+            corners.push_back({ bbox.xMin, bbox.yMin, bbox.zMin });
+            corners.push_back({ bbox.xMax, bbox.yMin, bbox.zMin });
+            corners.push_back({ bbox.xMin, bbox.yMax, bbox.zMin });
+            corners.push_back({ bbox.xMin, bbox.yMin, bbox.zMax });
+            corners.push_back({ bbox.xMax, bbox.yMax, bbox.zMin });
+            corners.push_back({ bbox.xMax, bbox.yMin, bbox.zMax });
+            corners.push_back({ bbox.xMin, bbox.yMax, bbox.zMax });
+            corners.push_back({ bbox.xMax, bbox.yMax, bbox.zMax });
+            return GeometricBox(corners);
+        }
+
+        SafePtr<TagNode> createTemperatureTag(Controller& controller, const PointXYZIRGB& point, double temperature, const char* colorName)
+        {
+            SafePtr<TagNode> tagNode = make_safe<TagNode>();
+            WritePtr<TagNode> wTag = tagNode.get();
+            if (!wTag)
+                return SafePtr<TagNode>();
+
+            const int digits = controller.cgetContext().m_unitUsage.displayedDigits;
+            const QString text = QStringLiteral("%1%2")
+                                     .arg(QString::number(temperature, 'f', digits), UnitConverter::getTemperatureUnitText());
+
+            wTag->setDefaultData(controller);
+            wTag->setVisible(true);
+            wTag->setPosition(glm::dvec3(point.x, point.y, point.z));
+            wTag->setMarkerIcon(scs::MarkerIcon::Tag_Base);
+            wTag->setColor(ProjectColor::getColor(colorName));
+            wTag->setName(text.toStdWString());
+            return tagNode;
+        }
+    }
+
     //
     // Click
     //
@@ -151,6 +205,127 @@ namespace control::picking
     ControlType PickColorimetricFromPick::getType() const
     {
         return ControlType::pickColorimetricFromPicking;
+    }
+
+    FindMinMaxTemperatureInClipping::FindMinMaxTemperatureInClipping()
+    {
+    }
+
+    FindMinMaxTemperatureInClipping::~FindMinMaxTemperatureInClipping()
+    {
+    }
+
+    void FindMinMaxTemperatureInClipping::doFunction(Controller& controller)
+    {
+        const GraphManager& graphManager = controller.cgetGraphManager();
+        const TemperatureScaleData temperatureScale = graphManager.getTemperatureScaleData();
+        if (!temperatureScale.isValid || temperatureScale.rgbToTemperature.empty())
+        {
+            controller.updateInfo(new GuiDataWarning(QObject::tr("You must import a valid temperature scale file first.")));
+            return;
+        }
+
+        const std::unordered_set<SafePtr<AClippingNode>> clippings = graphManager.getClippingObjects(true, false);
+        if (clippings.size() != 1)
+        {
+            controller.updateInfo(new GuiDataWarning(TEXT_TEMPERATURE_MINMAX_NEEDS_ONE_INTERIOR_CLIP));
+            return;
+        }
+
+        const SafePtr<AClippingNode>& clipping = *clippings.begin();
+        ReadPtr<AClippingNode> rClip = clipping.cget();
+        if (!rClip
+            || rClip->getType() != ElementType::Box
+            || rClip->getClippingMode() != ClippingMode::showInterior
+            || rClip->isVisible())
+        {
+            controller.updateInfo(new GuiDataWarning(TEXT_TEMPERATURE_MINMAX_NEEDS_ONE_INTERIOR_CLIP));
+            return;
+        }
+
+        controller.updateInfo(new GuiDataProcessingSplashScreenStart(1, QObject::tr("Find min max temperature"), QObject::tr("Please wait...")));
+        controller.updateInfo(new GuiDataProcessingSplashScreenEnableCancelButton(false));
+
+        ClippingAssembly clippingAssembly;
+        graphManager.getClippingAssembly(clippingAssembly, true, false);
+
+        TlScanOverseer::setWorkingScansTransfo(graphManager.getVisiblePointCloudInstances(tls::ScanGuid(), true, true));
+
+        const BoundingBoxD visibleScansBBox = graphManager.getScanBoundingBox(ObjectStatusFilter::VISIBLE);
+        if (!visibleScansBBox.isValid())
+        {
+            controller.updateInfo(new GuiDataProcessingSplashScreenEnd(QObject::tr("Done")));
+            controller.updateInfo(new GuiDataWarning(QObject::tr("No visible scan found in the project.")));
+            return;
+        }
+
+        const GeometricBox searchBox = makeGeometricBoxFromBoundingBox(visibleScansBBox);
+        std::vector<PointXYZIRGB> points;
+        TlScanOverseer::getInstance().collectPointsInGeometricBox(searchBox, clippingAssembly, tls::ScanGuid(), points);
+
+        bool minFound = false;
+        bool maxFound = false;
+        double minTemperature = std::numeric_limits<double>::max();
+        double maxTemperature = std::numeric_limits<double>::lowest();
+        PointXYZIRGB minPoint{};
+        PointXYZIRGB maxPoint{};
+
+        for (const PointXYZIRGB& point : points)
+        {
+            const uint32_t key = makeTemperatureScaleKey(point.r, point.g, point.b);
+            auto it = temperatureScale.rgbToTemperature.find(key);
+            if (it == temperatureScale.rgbToTemperature.end())
+                continue;
+
+            const double value = it->second;
+            if (!minFound || value < minTemperature)
+            {
+                minFound = true;
+                minTemperature = value;
+                minPoint = point;
+            }
+            if (!maxFound || value > maxTemperature)
+            {
+                maxFound = true;
+                maxTemperature = value;
+                maxPoint = point;
+            }
+        }
+
+        controller.updateInfo(new GuiDataProcessingSplashScreenProgressBarUpdate(QObject::tr("Done"), 1));
+        controller.updateInfo(new GuiDataProcessingSplashScreenEnd(QObject::tr("Done")));
+
+        if (!minFound || !maxFound)
+        {
+            controller.updateInfo(new GuiDataWarning(QObject::tr("No temperature from the current scale was found in the clipped area.")));
+            return;
+        }
+
+        SafePtr<TagNode> minTag = createTemperatureTag(controller, minPoint, minTemperature, "BLUE");
+        SafePtr<TagNode> maxTag = createTemperatureTag(controller, maxPoint, maxTemperature, "RED");
+
+        std::unordered_set<SafePtr<AGraphNode>> tagsToAdd;
+        if (minTag)
+            tagsToAdd.insert(minTag);
+        if (maxTag)
+            tagsToAdd.insert(maxTag);
+
+        if (!tagsToAdd.empty())
+            controller.getControlListener()->notifyUIControl(new control::function::AddNodes(tagsToAdd));
+    }
+
+    bool FindMinMaxTemperatureInClipping::canUndo() const
+    {
+        return false;
+    }
+
+    void FindMinMaxTemperatureInClipping::undoFunction(Controller& controller)
+    {
+    }
+
+    ControlType FindMinMaxTemperatureInClipping::getType() const
+    {
+        return ControlType::pickMinMaxTemperatureFromClipping;
     }
 
 }
