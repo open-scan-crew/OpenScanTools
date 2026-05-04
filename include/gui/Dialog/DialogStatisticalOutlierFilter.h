910134bc1c0a065e563497588df1040de8829d04
YanKoch <74600850+Yan-Koch@users.noreply.github.com>
2026-05-04 23:43:30 +0200

    Add filtering execution mode selection to SO/CB dialogs (#714)

--- include/gui/Dialog/DialogStatisticalOutlierFilter.h
+++ include/gui/Dialog/DialogStatisticalOutlierFilter.h
@@ -40,6 +40,7 @@ private:
     int m_samplingPercent = 2;
     double m_beta = 4.0;
     FileType m_outputFileType = FileType::TLS;
+    FilterExecutionMode m_executionMode = FilterExecutionMode::ExportFilteredAreas;
     std::wstring m_outputFolder;
     QString m_openPath;
 };
